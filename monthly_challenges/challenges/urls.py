24471fabc7ce3278c1379fbf81adbb375a840b95
Eddie Santiago <eddiesantiago122007@gmail.com>
2025-08-17 21:29:30 -0500

    ✨ feat: Add redirects and urls absolute path dinamically with the reverse function

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="challenges_dashboard"),
-    path("<int:month>/", views.monthly_challenge, name="monthly_challenges"), # Dynamic Path Segment
+    path("<int:month>/", views.monthly_challenge_by_number, name="monthly_challenges_by_number"), # Dynamic Path Segment
+    path("<str:month>/", views.monthly_challenge, name="monthly_challenges"), # Dynamic Path Segment
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,25 +1,35 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
-# Create your views here.
+# Challenges dictionary
+challenges = {
+        "january": "Walk 20 minutes every day",
+        "february": "Read 10 pages of a book daily",
+        "march": "Drink 2 liters of water every day",
+        "april": "Learn 5 new words in another language each day",
+        "may": "Wake up 30 minutes earlier than usual",
+        "june": "Do 15 minutes of stretching every morning",
+        "july": "Write a gratitude note daily",
+        "august": "Limit social media use to 30 minutes a day",
+        "september": "Eat at least one fruit and one vegetable daily",
+        "october": "Practice meditation for 10 minutes every day",
+        "november": "Do 20 push-ups each morning",
+        "december": "Reflect and write about your year for 5 minutes daily"
+}
+
 def dashboard(request):
     return HttpResponse("This is the challenges dashboard!")
 
 # Dynamic view
 def monthly_challenge(request, month):
-    challenges = {
-        "1": "Walk 20 minutes every day",
-        "2": "Read 10 pages of a book daily",
-        "3": "Drink 2 liters of water every day",
-        "4": "Learn 5 new words in another language each day",
-        "5": "Wake up 30 minutes earlier than usual",
-        "6": "Do 15 minutes of stretching every morning",
-        "7": "Write a gratitude note daily",
-        "8": "Limit social media use to 30 minutes a day",
-        "9": "Eat at least one fruit and one vegetable daily",
-        "10": "Practice meditation for 10 minutes every day",
-        "11": "Do 20 push-ups each morning",
-        "12": "Reflect and write about your year for 5 minutes daily"
-    }
+    return HttpResponse(f"The challenge is: { challenges.get(month, 'Month not found') }")
 
-    return HttpResponse(f"The challenge is: { challenges.get(str(month), 'Month not found') }")
+def monthly_challenge_by_number(request, month):
+    months = list(challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    redirect_month = months[month - 1]
+    redirect_path = reverse("monthly_challenges", args=[redirect_month]) # Build the absolute path dinamically
+    return HttpResponseRedirect(redirect_path)
