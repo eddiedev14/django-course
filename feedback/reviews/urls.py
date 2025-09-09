fbdcc6d46765c14c80a6e4f0da4c7822e8848dc3
Eddie Santiago <eddiesantiago122007@gmail.com>
2025-09-09 14:57:02 -0500

    ✨ feat: Finish Forms Lesson

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review, name='form')
+    path('', views.review, name='form'),
+    path('thank_you/', views.thank_you, name='thank_you')
 ]
